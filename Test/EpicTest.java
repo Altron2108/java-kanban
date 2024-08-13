c99f9761025307427f87886fada4862a92ad0fbc
Dmitry Yarovoy <altron2108@yandex.ru>
2024-08-13 20:34:54 +0300

    Этот класс тестирует работу с эпиками, включая добавление и удаление подзадач, а также корректное вычисление статуса эпика на основе его подзадач.

---
+++ Test/EpicTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class EpicTest {
+    @Test
+    public void testEpicConstructor() {
+        Epic epic = new Epic("Epic Title", "Epic Description");
+        assertEquals("Epic Title", epic.getTitle());
+        assertEquals("Epic Description", epic.getDescription());
+        assertEquals(Status.NEW, epic.getStatus());
+    }
+
+    @Test
+    public void testAddSubtask() {
+        Epic epic = new Epic("Epic Title", "Epic Description");
+        int subtaskId = 1;
+        epic.addSubtask(subtaskId);
+        assertTrue(epic.getSubtaskIds().contains(subtaskId));
+    }
+}
