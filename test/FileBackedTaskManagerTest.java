05a25b1831cad4937da0aa4b0631e0fbb326a601
Dmitry Yarovoy <altron2108@yandex.ru>
2024-10-19 11:09:03 +0300

    - Добавлен тест для сохранения и загрузки задач из файла (saveAndLoadTasks_WhenTaskIsCreated_ShouldSaveAndLoadTasksCorrectly). - Добавлен тест для проверки сортировки задач по приоритету (getPrioritizedTasks_ShouldReturnTasksInOrder). - Обновлен метод setUp() для инициализации FileBackedTaskManager с тестовым файлом.

--- test/FileBackedTaskManagerTest.java
+++ test/FileBackedTaskManagerTest.java
@@ -19,12 +19,12 @@ public class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskMan
     }
 
     @Test
-    public void testSaveAndLoadTasks() throws IOException {
+    public void saveAndLoadTasks_WhenTaskIsCreated_ShouldSaveAndLoadTasksCorrectly() throws IOException {
         // Создание задачи и сохранение в файл
         Task task = new RegularTask("Task 1", "Description 1", Status.NEW,
                 Duration.ofMinutes(60), LocalDateTime.of(2024, 10, 15, 16, 0));
         taskManager.createTask(task);
-        taskManager.save();
+
 
         // Создаем новый менеджер и загружаем данные из файла
         FileBackedTaskManager newManager = new FileBackedTaskManager(testFile);
