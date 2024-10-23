8c8c36bd9708d036536a9c92332e862dde1bcda3
Dmitry Yarovoy <altron2108@yandex.ru>
2024-10-23 23:11:39 +0300

    - Добавлены методы для управления задачами, эпиками и подзадачами, включая создание, получение, обновление и удаление элементов. - Введён метод `getPrioritizedTasks` для получения списка задач по приоритету. - Добавлен метод `getHistory` для получения истории просмотренных задач. - Упорядочены методы для лучшего восприятия и структурирования интерфейса.

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,27 +1,29 @@
 import java.util.List;
 
 public interface TaskManager {
+    void addTask(Task task);
+
     // Методы для управления задачами
     int createTask(Task task);             // Создание задачи
 
-    Task getTaskById(int id);
+    Task getTaskById(int id);              // Получение задачи по ID
 
     void updateTask(Task task);            // Обновление задачи
 
-    void deleteTaskById(int id);
+    void deleteTaskById(int id);           // Удаление задачи по ID
 
     void removeAllTasks();                 // Удаление всех задач
 
     List<Task> getTasks();                 // Получение всех задач
 
     // Методы для управления эпиками
-    void createEpic(Epic epic);             // Создание эпика
+    void createEpic(Epic epic);            // Создание эпика
 
-    Epic getEpicById(int id);
+    Epic getEpicById(int id);              // Получение эпика по ID
 
     void updateEpic(Epic epic);            // Обновление эпика
 
-    void deleteEpicById(int id);
+    void deleteEpicById(int id);           // Удаление эпика по ID
 
     void removeAllEpics();                 // Удаление всех эпиков
 
@@ -30,16 +32,19 @@ public interface TaskManager {
     // Методы для управления подзадачами
     void createSubtask(Subtask subtask);    // Создание подзадачи
 
-    Subtask getSubtaskById(int id);
+    Subtask getSubtaskById(int id);        // Получение подзадачи по ID
 
     void updateSubtask(Subtask subtask);   // Обновление подзадачи
 
-    void deleteSubtaskById(int id);
+    void deleteSubtaskById(int id);        // Удаление подзадачи по ID
 
-    void removeAllSubtasks();              // Удаление всех подзадач
+    void removeAllSubtasks();               // Удаление всех подзадач
 
     List<Subtask> getSubtasks();           // Получение всех подзадач
 
     // Получение списка задач по приоритету
-    List<Task> getPrioritizedTasks();
+    List<Task> getPrioritizedTasks();       // Получение задач по приоритету
+
+    // Получение истории задач
+    List<Task> getHistory();                // Получение истории просмотренных задач
 }
